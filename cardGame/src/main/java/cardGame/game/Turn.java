343001bbc25dfaa3b6f92dd1336286533e2b7688
Warner Lensing <Warner@RN-145-90-152-136.eduroam.rug.nl>
2019-05-13 22:02:09 +0200

    added a little to turn

--- cardGame/src/main/java/cardGame/game/Turn.java
+++ cardGame/src/main/java/cardGame/game/Turn.java
@@ -4,10 +4,11 @@ import cardGame.models.Card;
 
 public class Turn {
 
+    private int numWrongGuesses;
 
     public void Turn(Card.Face chosenCardPlayer, Card.Face chosenCardDealer){
         if(chosenCardPlayer == chosenCardDealer){
-            System.out.println("You've guessed the right face, I have to drink.");
+            System.out.println("You've guessed the right face");
         } else if(chosenCardPlayer.ordinal() < chosenCardPlayer.ordinal()){
             System.out.println("The card in my hand is higher than your guess.");
         } else{
@@ -17,11 +18,16 @@ public class Turn {
         Card.Face guessFace2 = Card.Face.ACE ; //get new guess
 
         if(guessFace2 != chosenCardDealer){
-            System.out.println("You've guesses the wrong card again, you have to drink...");
+            System.out.println("You've guesses the wrong face again, you have to drink...");
+            numWrongGuesses++;
+            if(numWrongGuesses==3){
+                System.out.println("Three persons have guessed wrong, so you can give the cards to the person next to you");
+                // TODO: person next to you has to be given
+                numWrongGuesses = 0;
+            }
         } else{
-            System.out.println("Buggers, you've guessed the right card, I have to drink...");
+            System.out.println("Buggers, you've guessed the right face, I have to drink...");
         }
-
     }
 
 
